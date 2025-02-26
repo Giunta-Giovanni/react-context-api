24ef1198e275aa68338aa9e7a2393e346235cabf
GiuntaGiovanni <jhonniest741@gmail.com>
2025-02-26 15:54:41 +0100

    [feat] definition of context post

---
+++ src/context/PostContext.jsx
@@ -0,0 +1,8 @@
+//importiamo {createContext} da react
+import { createContext } from "react";
+
+// Creiamo il nostro <PostContex = createContext>
+const PostContext = createContext
+
+// Lo esportiamo
+export default PostContext;
