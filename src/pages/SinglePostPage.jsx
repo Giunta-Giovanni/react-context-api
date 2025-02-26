8344facd1bb9dfc34c1c93af611a1c6823bca045
GiuntaGiovanni <jhonniest741@gmail.com>
2025-02-26 17:44:34 +0100

    [feat] sinfle post page online

--- src/pages/SinglePostPage.jsx
+++ src/pages/SinglePostPage.jsx
@@ -3,8 +3,7 @@ import SinglePost from "../components/Post-components/SinglePost";
 export default function SinglePostPage() {
 
     return (
-        <div>questa è la pagina del singolo post</div>
-        // <SinglePost />
+        <SinglePost />
     )
 
 }
